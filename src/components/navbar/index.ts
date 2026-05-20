660784d32e8f1fbad58ff2c44727ad9e95731979
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-05-21 01:17:55 +0200

    feat(navbar): refactor to sva slot recipe with createStyleContext

---
+++ src/components/navbar/index.ts
@@ -0,0 +1,8 @@
+export type {
+	NavbarActionsProps,
+	NavbarBrandProps,
+	NavbarLinksProps,
+	NavbarMobileMenuProps,
+	NavbarRootProps,
+} from "./navbar";
+export { Navbar, useNavbar } from "./navbar";
